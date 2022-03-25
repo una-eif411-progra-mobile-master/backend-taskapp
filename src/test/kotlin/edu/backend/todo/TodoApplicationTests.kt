adc446dd5834dac292a1ce0116776c0d86f85da1
Maikol Guzman <mikeguzman@gmail.com>
2022-03-24 19:32:13 -0600

    Version inicial del Backend

---
+++ src/test/kotlin/edu/backend/todo/TodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.backend.todo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
