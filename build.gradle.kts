987c2375ce383cc7cfbc7e2e023c2ca2c019dfbe
Maikol Guzman <mikeguzman@gmail.com>
2022-10-06 20:01:01 -0600

    Mapstruct updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,9 @@ repositories {
 }
 
 dependencies {
-	implementation ("org.mapstruct:mapstruct:1.5.0.RC1")
-	kapt("org.mapstruct:mapstruct-processor:1.5.0.RC1")
-	annotationProcessor ("org.mapstruct:mapstruct:1.5.0.RC1")
+	implementation ("org.mapstruct:mapstruct:1.5.2.Final")
+	kapt("org.mapstruct:mapstruct-processor:1.5.2.Final")
+	annotationProcessor ("org.mapstruct:mapstruct:1.5.2.Final")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
