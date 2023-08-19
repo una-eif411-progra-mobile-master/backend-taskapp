b7ae61f5a6bce3d538db35477d5a80c40ff871f2
Maikol Guzmán Alán <mikeguzman@gmail.com>
2023-08-18 23:28:11 -0600

    Migration from Spring Boot 2 to 3
    - Migration from Java 11 to Java 17

--- src/main/kotlin/edu/backend/taskapp/Entities.kt
+++ src/main/kotlin/edu/backend/taskapp/Entities.kt
@@ -1,7 +1,7 @@
 package edu.backend.taskapp
 
+import jakarta.persistence.*
 import java.util.*
-import javax.persistence.*
 
 @Entity
 @Table(name = "reminder")
@@ -58,7 +58,7 @@ data class Task(
     @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "id")
     var user: User,
 
-) {
+    ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Task) return false
@@ -151,9 +151,9 @@ data class Privilege(
     var id:Long? = null,
     var name: String,
     // Entity Relationship
-    @ManyToMany(mappedBy = "roleList", fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY)
     var userList: Set<User>,
-    @ManyToMany(mappedBy = "privilegeList", fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY)
     var roleList: Set<Role>,
 
 ) {
