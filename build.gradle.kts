a4e817e6e9d47173d9813b771a30defa45579a80
Maikol Guzmán Alán <mikeguzman@gmail.com>
2023-09-24 01:55:36 -0600

    Dependencies updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.2"
+	id("org.springframework.boot") version "3.1.4"
 	id("io.spring.dependency-management") version "1.1.2"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
-	kotlin("plugin.jpa") version "1.9.0"
-	kotlin("kapt") version "1.9.0"
+	kotlin("plugin.jpa") version "1.9.10"
+	kotlin("kapt") version "1.9.10"
 }
 
 group = "edu.backend"
